04f52c3be8885a030660de248d389f5a7528d1d1
RyuuShiro30 <annoraegaprasetyo@gmail.com>
2025-11-15 12:29:38 +0700

    kerangka about lab ivss

---
+++ modul/index.php
@@ -0,0 +1,45 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intelligent Vision and Smart System</title>
+    <link rel="stylesheet" href="../style/navbar.css">
+    <link rel="stylesheet" href="../style/style_index.css">
+</head>
+<body>
+
+<header class="navbar">
+    <div class="logo-area">
+        <img src="../img/polinema.png" alt="Logo" class="logo">
+        <span class="site-title">POLITEKNIK NEGERI MALANG</span>
+    </div>
+
+    <nav class="menu">
+        <a href="">Home</a>
+        <a href="about.php">About</a>
+        <a href="#">News</a>
+        <a href="#">Members</a>
+        <a href="#">Research</a>
+    </nav>
+</header>
+
+<section class="hero">
+    <h1>INTELLIGENT VISION<br>AND SMART SYSTEM</h1>
+    <p>Shaping the Future through Intelligent Vision and Adaptive Systems</p>
+</section>
+
+<div class="learn-more">
+    <a href="about.php" class="learn-more-link">
+        <h2>
+            <img src="../img/right-arrow.png" alt="">
+            Learn More 
+        </h2>
+    </a>
+</div>
+
+</body>
+</html>
